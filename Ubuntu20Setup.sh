9d00a4e91658a03329faa87dd439a97f235fb25b
alexlewislnk <39069234+alexlewislnk@users.noreply.github.com>
2021-08-17 15:08:08 -0500

    Update and rename Ubuntu20Setup to Ubuntu20Setup.sh

--- Ubuntu20Setup.sh
+++ Ubuntu20Setup.sh
@@ -48,6 +48,9 @@ sed -i 's/^HostKey \/etc\/ssh\/ssh_host_\(dsa\|ecdsa\)_key$/\#HostKey \/etc\/ssh
 echo -e "\n# Restrict key exchange, cipher, and MAC algorithms\nKexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256\nCiphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr\nMACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com\nHostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-512,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-512-cert-v01@openssh.com" > /etc/ssh/sshd_config.d/ssh-audit_hardening.conf
 awk '$5 >= 3071' /etc/ssh/moduli > /etc/ssh/moduli.safe >>$LOG 2>&1
 mv /etc/ssh/moduli.safe /etc/ssh/moduli
+rm /etc/ssh/ssh_host_* >>$LOG 2>&1
+ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N "" >>$LOG 2>&1
+ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N "" >>$LOG 2>&1
 service ssh restart >>$LOG 2>&1
 ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa <<<y >>$LOG 2>&1
 ssh-keygen -t ed25519 -N '' -f ~/.ssh/id_ed25519 <<<y >>$LOG 2>&1
@@ -133,7 +136,7 @@ Unattended-Upgrade::Automatic-Reboot "true";
 Unattended-Upgrade::Automatic-Reboot-Time "02:00";
 EOF
 rm -rf /etc/systemd/system/apt-daily.timer* >>$LOG 2>&1
-cat > /etc/systemd/system/apt-daily.timer >>EOF
+cat > /etc/systemd/system/apt-daily.timer <<EOF
 [Unit]
 Description=Daily apt download activities
 [Timer]
